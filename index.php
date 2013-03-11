3fd81530b7378b8a6b49c527e57c1f37cdf34b8a
Adam Brewer <adamcbrewer@gmail.com>
2013-03-11 07:33:44 +0000

    Changed github icon

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 
 			<footer class="footer">
 				<a class="icon icon-twitter" href="http://twitter.com/adamcbrewer" target="_blank" title="Twitter"><span>Twitter</span></a>
-				<a class="icon icon-github" href="http://github.com/adamcbrewer" target="_blank" title="Github"><span>Github</span></a>
+				<a class="icon icon-github-circled" href="http://github.com/adamcbrewer" target="_blank" title="Github"><span>Github</span></a>
 				<a class="icon icon-linkedin" href="http://za.linkedin.com/in/adamcbrewer" target="_blank" title="LinkedIn"><span>LinkedIn</span></a>
 				<a class="icon icon-user" href="http://adamcbrewer.com" target="_blank" title="adamcbrewer.com"><span>adamcbrewer.com</span></a>
 				<a class="icon icon-gplus" href="https://plus.google.com/u/0/109651434155076896860/" target="_blank" title="Google Plus"><span>Google +</span></a>
