aa9516106faec76a015ba32d18d06d5566724cc9
Adam Brewer <adamcbrewer@gmail.com>
2013-03-15 07:21:20 +0000

    Removed some H5BP header defaults

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <? require("config.php"); ?>
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1,requiresActiveX=true">
