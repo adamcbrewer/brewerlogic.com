d29b599c84431875dedeb4564a9f9b9c72935c32
Adam Brewer <adamcbrewer@gmail.com>
2013-06-30 13:24:47 +0100

    Portfolio in progression

--- index.php
+++ index.php
@@ -1,88 +1,9 @@
-<? require("config.php"); ?>
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1,requiresActiveX=true">
-	<meta name="author" content="">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>Brewer Logic</title>
-
-	<meta name="robots" content="all">
-
-	<link type="text/plain" rel="author" href="humans.txt" />
-
-	<meta name="description" content="The work of web developer, Adam Brewer">
-	<meta name="keywords" content="brewerlogic, adamcbrewer, adam brewer, brewer logic, web-development, front-end developer, front end developer">
-
-	<meta property="og:title" content="Brewer Logic"/>
-	<meta property="og:type" content="Portfolio"/>
-	<meta property="og:image" content="assets/img/logo_a.png"/>
-	<meta property="og:url" content="http://brewerlogic.com"/>
-	<meta property="og:site_name" content="Brewer Logic"/>
-	<meta property="og:description" content="The work of web developer, Adam Brewer"/>
-
-	<link href="assets/css/core.css" rel="stylesheet">
-
-	<? //<script src="assets/js/libs/modernizr-2.6.2.custom.js"></script> ?>
-
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-31471338-1']);
-		_gaq.push(['_trackPageview']);
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	</script>
-
-</head>
-<body data-base-url="<?= $site_url; ?>">
-
-	<div class="wrapper">
-
-		<article class="content">
-			<h1 class="logo"><a href="/" title="Brewer Logic">Brewer Logic</a></h1>
-			<div class="spinner">
-				<div class="cube">
-					<div class="side side-front"></div>
-					<div class="side side-bottom"></div>
-					<div class="side side-back"></div>
-					<div class="side side-top"></div>
-					<div class="side side-left"></div>
-					<div class="side side-right"></div>
-				</div>
-			</div>
-			<p class="intro">coming soon</p>
-
-			<footer class="footer">
-				<a class="icon icon-twitter" href="http://twitter.com/adamcbrewer" target="_blank" title="Twitter"><span>Twitter</span></a>
-				<a class="icon icon-github-circled" href="http://github.com/adamcbrewer" target="_blank" title="Github"><span>Github</span></a>
-				<a class="icon icon-linkedin" href="http://za.linkedin.com/in/adamcbrewer" target="_blank" title="LinkedIn"><span>LinkedIn</span></a>
-				<a class="icon icon-user" href="http://adamcbrewer.com" target="_blank" title="adamcbrewer.com"><span>adamcbrewer.com</span></a>
-				<a class="icon icon-gplus" href="https://plus.google.com/u/0/109651434155076896860/" target="_blank" title="Google Plus"><span>Google +</span></a>
-			</footer>
-		</article>
-
-	</div>
-
-<!--
-
-___________________________________________________________________________________________________________________________________________________
- ___________________________________________________________________________________________________________________________________________________
-  __________________________________________________/\\\_________________/\\\\\\\\___________________________________________________________________
-   _____/\\\\\\\\_____/\\\\\_______/\\\\\__/\\\\\___\///___/\\/\\\\\\____/\\\////\\\____________/\\\\\\\\\\_____/\\\\\________/\\\\\_____/\\/\\\\\\___
-    ___/\\\//////____/\\\///\\\___/\\\///\\\\\///\\\__/\\\_\/\\\////\\\__\//\\\\\\\\\___________\/\\\//////____/\\\///\\\____/\\\///\\\__\/\\\////\\\__
-     __/\\\__________/\\\__\//\\\_\/\\\_\//\\\__\/\\\_\/\\\_\/\\\__\//\\\__\///////\\\___________\/\\\\\\\\\\__/\\\__\//\\\__/\\\__\//\\\_\/\\\__\//\\\_
-      _\//\\\________\//\\\__/\\\__\/\\\__\/\\\__\/\\\_\/\\\_\/\\\___\/\\\__/\\_____\\\___________\////////\\\_\//\\\__/\\\__\//\\\__/\\\__\/\\\___\/\\\_
-       __\///\\\\\\\\__\///\\\\\/___\/\\\__\/\\\__\/\\\_\/\\\_\/\\\___\/\\\_\//\\\\\\\\_____________/\\\\\\\\\\__\///\\\\\/____\///\\\\\/___\/\\\___\/\\\_
-        ____\////////_____\/////_____\///___\///___\///__\///__\///____\///___\////////_____________\//////////_____\/////________\/////_____\///____\///__
-
-
--->
-
-</body>
-</html>
+<? include($_SERVER['DOCUMENT_ROOT'] . "/partials/header.php"); ?>
+<ul>
+	<li class="work-item">
+		<h1 class="work-item-header">
+			<a class="work-link" href="<?= $config->work ?>/brewers-droop">Brewers Droop</a>
+		</h1>
+	</li>
+</ul>
+<? include($_SERVER['DOCUMENT_ROOT'] . "/partials/footer.php"); ?>
---
+++ work/brewers-droop/index.php
@@ -0,0 +1,7 @@
+<? include($_SERVER['DOCUMENT_ROOT'] . "/partials/header.php"); ?>
+<header>
+	<h1 class="work-item-header">
+		Brewers Droop
+	</h1>
+</header>
+<? include($_SERVER['DOCUMENT_ROOT'] . "/partials/footer.php"); ?>
