f2878df6a53a12f5e33fc961d5b2715a1faa90d4
adam.brewer <adam.brewer@burberry.com>
2014-12-09 15:36:43 +0000

    Twitter API working and filtering by param in CMS

--- app/site/config/config.php
+++ app/site/config/config.php
@@ -16,8 +16,6 @@ for more information: http://getkirby.com/license
 */
 
 c::set('license', 'put your license key here');
-c::set('debug', false);
-c::set('cache', true);
 
 /*
 
@@ -30,3 +28,75 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+c::set('debug', false);
+c::set('cache', true);
+
+c::set('twitter', array(
+    'consumer_key'      => '',
+    'consumer_secret'   => '',
+    'user_token'        => '',
+    'user_secret'       => '',
+    'screen_name'       => ''
+));
+
+/*
+
+---------------------------------------
+Routing
+---------------------------------------
+
+*/
+
+c::set('routes', array(
+    array(
+        'pattern' => 'api/twitter',
+        'method' => 'GET',
+        'action' => function () {
+
+            if(!r::ajax()) {
+                header::notfound();
+                return;
+            }
+
+            $response = array(
+                'status' => false,
+                'tweets' => null,
+                'filter' => null
+            );
+
+            $site = site();
+            $filter = null;
+
+            $vendor = $site->kirby()->roots()->index() . DS . 'vendor';
+            require($vendor . DS . 'tmhOAuth.php');
+            require($vendor . DS . 'twitterApi.php');
+
+            if (!$site->tweetfilter()->empty()) $filter = (string) $site->tweetfilter();
+
+            $twitter_config = c::get('twitter');
+            $twitter = new Twitter($twitter_config);
+
+            if ($filter) {
+                $tweets = $twitter->search(20, 'from:' . $twitter_config['screen_name'] . ' ' . $filter);
+            } else {
+                $tweets = $twitter->get(20);
+            }
+
+            $response['filter'] = $filter;
+            if ($tweets) {
+                $response['status'] = true;
+
+                if (isset($tweets->statuses)) {
+                    $tweets = $tweets->statuses;
+                }
+
+                $response['tweets'] = $tweets;
+            }
+
+            header::contentType('application/json');
+            return response::json($response);
+        }
+    )
+));
+
