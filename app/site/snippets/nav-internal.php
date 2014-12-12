eb074005a4fa32af5093e8f887e820cdf8b309f7
adam.brewer <adam.brewer@burberry.com>
2014-12-12 13:44:52 +0000

    Some styles and moving navs to snippets

--- app/site/snippets/nav-internal.php
+++ app/site/snippets/nav-internal.php
@@ -1,3 +1,3 @@
 <?php foreach($pages->find('work', 'contact', 'about')->sortBy('num')->visible() as $page): ?>
-    <a class="nav-link" href="<?php echo $page->url() ?>"><?php echo $page->title()->html() ?></a>
+    <a class="nav-link<?php if ($page->isOpen()) : ?> is-active<?php endif; ?>" href="<?php echo $page->url() ?>"><?php echo $page->title()->html() ?></a>
 <?php endforeach; ?>
