c0a02ec42e2d0858b64bd697698073210fc43e21
Adam Brewer <adamcbrewer@gmail.com>
2015-02-22 10:53:42 +0000

    Build assets in header and footer

--- app/site/snippets/header.php
+++ app/site/snippets/header.php
@@ -26,7 +26,7 @@
         WebFont.load({typekit: { id: 'qwc7wds' }});
     </script>
 
-    <?php if (c::get('debug')) : echo css('assets/css/styles.prefixed.css'); else : echo css('assets/css/styles.css'); endif; ?>
+    <?php if (c::get('debug')) : echo css('assets/css/styles.css'); else : echo css('assets/css/styles.css'); endif; ?>
     <?php echo js('assets/js/modernizr.build.js') ?>
 
 </head>
