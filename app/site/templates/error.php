e5c260bbadd428fc85ea073466fd54824e41067c
Adam Brewer <adamcbrewer@gmail.com>
2015-02-22 09:51:21 +0000

    Fixed konami code and 404 video upload

--- app/site/templates/error.php
+++ app/site/templates/error.php
@@ -1,9 +1,12 @@
 <?php snippet('header') ?>
 
     <div class="fourohfour">
+
+        <?php if ($page->hasVideos() && $page->videos()) : ?>
         <div class="u-rv u-grain">
-            <video src="<?php echo $site->url() ?>/assets/video/gameover.mp4" autoplay></video>
+            <video src="<?php echo $page->videos()->first()->url() ?>" autoplay></video>
         </div>
+        <?php endif; ?>
 
         <footer class="fourohfour-footer">
             <a href="<?php echo $site->url() ?>" class="btn btn--primary">Continue</a> &nbsp; <a href="<?php echo $site->url() ?>" class="btn">Exit</a>
